44fe9cb040a1bfdde47a8db0bfe4fabdab5f6c8d
Arrubo-ru <Arrubo@yandex.ru>
2017-12-22 00:57:36 +0500

    s

---
+++ qq.java
@@ -0,0 +1,11 @@
+public class qq {
+public static void main (String args[]){
+
+int first = Integer.parseInt(args[0]);
+int second = Integer.parseInt(args[1]);
+int sum = first+second;
+System.out.println(sum);
+
+}
+
+}
