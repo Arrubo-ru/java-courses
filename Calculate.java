ed3e0e464e368a3d3219999f31c0de3b37b790cc
Arrubo-ru <Arrubo@yandex.ru>
2017-12-30 05:19:44 +0500

    New vers

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,13 @@
 public class Calculate {
 public static void main (String[] arg){
 System.out.println("Load sucess.");
+int a = Integer.valueOf(arg[0]);
+int b = Integer.valueOf(arg[1]);
+System.out.println("Результат сложения " + (a+b));
+System.out.println("Результат вычитания " + (a-b));
+System.out.println("Результат умножения " + (a*b));
+System.out.println("Результат деления " + (a/b));
+System.out.println("Результат возведения в степень " + Math.pow(a,b));
 
 }
 }
